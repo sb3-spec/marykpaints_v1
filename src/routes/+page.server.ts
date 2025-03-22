f8c3cac4bcf2ce9eeb1b5a1a1a987bb1f9deae49
Robert Sheffield <sheffira2@gmail.com>
2025-03-22 03:04:12 -0700

    Fiddling with prod env vars

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,12 @@
 import type { PageServerLoad } from './$types';
 import { POCKETBASE_PASSWORD } from '$env/static/private';
+import dotenv from 'dotenv';
 
 import PocketBase from 'pocketbase';
 
 export const load: PageServerLoad = async () => {
+	dotenv.config();
+
 	const pb = new PocketBase('https://pocketbase-production-7a10.up.railway.app');
 
 	await pb.collection('_superusers').authWithPassword('sheffira2@gmail.com', POCKETBASE_PASSWORD);
