53c710a84bef5300783006c1a92435da5c7b3b6c
Robert Sheffield <sheffira2@gmail.com>
2025-03-29 19:47:25 -0700

    Worked on layout for the portfolio page, will have 3 per row @lg, 2 for @md, and 1 for @sm

--- src/routes/paintings/+page.svelte
+++ src/routes/paintings/+page.svelte
@@ -4,26 +4,28 @@
 	let { data }: PageProps = $props();
 </script>
 
-<div class=" min-w-screen pt-[84px] text-center">
+<div class="overflow-x-hidden py-[84px] text-center">
 	<h1
-		class="text-text-primary mx-auto mb-10 w-10/12 border-b-2 pb-8 text-5xl lg:max-w-[58rem] lg:min-w-7/12 lg:text-7xl"
+		class="text-text-primary mx-auto mb-10 w-10/12 overflow-x-hidden border-b-2 pb-8 text-5xl lg:max-w-[58rem] lg:min-w-7/12 lg:text-7xl"
 	>
 		Paintings
 	</h1>
-	<ul class="mx-auto flex flex-wrap gap-2 lg:w-7/12">
+	<ul
+		class="lg:min-w-7/12l mx-auto flex w-5/6 flex-wrap justify-between gap-y-4 overflow-hidden lg:max-w-[58rem] lg:min-w-7/12 lg:gap-y-6"
+	>
 		{#each data.images as image}
-			<li class="relative sm:mx-auto">
+			<li class="relative md:basis-[49%] lg:basis-[32%]">
 				<a href={`/paintings/${image.id}`}>
 					<div
-						class="bg-gold-opaque absolute h-[34rem] w-full pt-20 opacity-0 transition duration-500 hover:opacity-100"
+						class="bg-gold-opaque absolute h-full w-full pt-20 opacity-0 transition duration-500 hover:opacity-100"
 					>
-						<h1 class="px-10 text-center text-7xl text-white">{image.name}</h1>
+						<h1 class="px-10 text-center text-5xl text-white">{image.name}</h1>
 					</div>
 				</a>
 				<img
 					src={`https://pocketbase-production-7a10.up.railway.app/api/files/pbc_3607937828/${image.id}/${image.file_name}?token=`}
 					alt={image.name}
-					class="object-cover"
+					class="object-cover lg:min-h-[25rem]"
 				/>
 			</li>
 		{/each}
